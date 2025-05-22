a7dbdc13a4e603a9dd38fe96540b84539d407c2f
iangmitchell <ian.g.mitchell@proton.me>
2025-05-22 17:08:42 +0100

    vite comply

---
+++ ctr-erc20/Header.jsx
@@ -0,0 +1,19 @@
+
+import 'bulma/css/bulma.min.css';
+function Header(props){
+  return(
+    <>
+      <div className="block">
+        <section className="hero is-info">
+         <div className="hero-body">
+          <div className="container">
+              <h1 className="title" > Increment and Decrement Token</h1> 
+              <h2 className="subtitle">Name: {props.name} Symbol:{props.symbol} </h2>
+          </div> 
+        </div> 
+        </section>   
+      </div> 
+    </>
+  )
+}
+export default Header;
